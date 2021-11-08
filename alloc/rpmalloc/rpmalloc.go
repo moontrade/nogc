f21f3b2d5b009ecc6e7231727680c70b63f78b8e
cmo <c@moontrade.io>
2021-11-08 10:43:54 +0800

    add darwin arm64 support

--- alloc/rpmalloc/rpmalloc.go
+++ alloc/rpmalloc/rpmalloc.go
@@ -6,6 +6,8 @@ package rpmalloc
 /*
 #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/lib/darwin_amd64 -L${SRCDIR}/lib/darwin_amd64
 #cgo darwin,amd64 LDFLAGS: -lrpmalloc -ldl -lc -lm
+#cgo darwin,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR}/lib/darwin_arm64 -L${SRCDIR}/lib/darwin_arm64
+#cgo darwin,arm64 LDFLAGS: -lrpmalloc -ldl -lc -lm
 #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/lib/linux_amd64 -L${SRCDIR}/lib/linux_amd64
 #cgo linux,amd64 LDFLAGS: -lrpmalloc -ldl -lc -lm
 #cgo linux,amd64 CFLAGS: -D_GNU_SOURCE
